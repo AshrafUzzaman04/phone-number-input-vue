811a1f59a4d22379486087c95e6d3fe8ba67fc90
Ashraf Uzzaman <ashraf.uzzaman04082004@gmail.com>
2024-09-09 03:33:30 +0600

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  modules: [
+    {
+      publicPath: process.env.NODE_ENV === 'production'
+        ? '/phone-number-input-vue/'
+        : '/'
+    }
+  ]
 })
